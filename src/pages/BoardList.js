fb2ad2a62f5d5e33a78fee939447a5afdebd3264
BeTweak <ys@betweak.com>
2020-12-20 18:36:16 +0900

    fix solidity for getCheckAll

--- src/pages/BoardList.js
+++ src/pages/BoardList.js
@@ -1,10 +1,40 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {Container, Row, Col, Button, ListGroup} from 'react-bootstrap'
 import {Link} from 'react-router-dom';
 
 export default function BoardList(props){
   console.log(props);
   const {history, match, location} = props;
+
+
+  const [boardList, setBoardList] = useState([]);
+  
+  useEffect(()=>{
+    // 1. 서버와 통신 (GET요청)
+
+    // 비동기로 요청을 보낸다.
+
+    // fetch('http://localhost:3000/api/board', {
+    // TODO: CORS 에러 발생 (설명 필요.)
+
+    fetch('/api/board', {
+      method: 'GET'
+    }).then(resp=>{
+      return resp.json();
+    }).then(data=>{
+      console.log(data);
+      if (data.status === 'error'){
+        alert("Server Error");
+        alert(data.result);
+      } else{
+        setBoardList(data.result);
+      }
+    })
+
+    // 2. 서버에서 데이터를 받아온 후 boardList라는 State에 저장!
+  }, [])
+
+
   return (
     <Container style={{paddingTop: 60}}>
       <Row>
@@ -15,6 +45,7 @@ export default function BoardList(props){
 
       <Row style={{paddingTop: 50, paddingBottom:50}}>
         <Col>
+          
           {/* Button "클릭"시 "/board/write르 이동시키기" */}
           {/* 2가지방법. */}
 
@@ -37,8 +68,11 @@ export default function BoardList(props){
       <Row>
         <Col>
           <ListGroup>
-            <ListGroup.Item>게시글1</ListGroup.Item>
-            <ListGroup.Item>게시글2</ListGroup.Item>
+            {boardList.map(post=>{
+              return (<ListGroup.Item key={post.id}>{post.title}</ListGroup.Item>)
+            })}
+            {/* <ListGroup.Item>게시글1</ListGroup.Item>
+            <ListGroup.Item>게시글2</ListGroup.Item> */}
           </ListGroup>
         </Col>
       </Row>
